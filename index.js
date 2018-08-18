921f17842031c6390e415306285976ace8982acf
Nacho <ignaciomies@live.com>
2018-08-18 12:36:40 -0300

    implementacion del for

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-for(var i = 0; i <= 10; i++)
-{
-    console.log('numero '+i);
-}
+//BUCLES
+
+// for(var i = 0; i <= 10; i++)
+// {
+//     console.log('numero ' + i);
+// }
+
+// var daysOfWeek = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+// for(var i  = daysOfWeek.length -1; i >= 0; i--)
+// {
+//     console.log(daysOfWeek[i]);
+// }
+
